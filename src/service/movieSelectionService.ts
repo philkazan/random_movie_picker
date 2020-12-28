c36d86b198eccb468938e32c1dece2b8ed9f999d
philkazan <philkazan@gmail.com>
2020-12-28 17:06:27 -0500

    add relative uris that point to input file

--- src/service/movieSelectionService.ts
+++ src/service/movieSelectionService.ts
@@ -2,12 +2,12 @@ import { Movie } from '../resource/movie';
 
 export class MovieSelectionService { 
     getAvailableMovies() {
-        const inputFile: Movie[] = require('../input/movies.json');
+        const inputFile: Movie[] = require('../../input/movies.json');
         return inputFile;
     }
 
     getRandomMovie() {
-        const availableMovies: Movie[] = require('../input/movies.json');
+        const availableMovies: Movie[] = require('../../input/movies.json');
         const randomIndex = Math.floor(Math.random() * availableMovies.length)
         return availableMovies[randomIndex];
     }
