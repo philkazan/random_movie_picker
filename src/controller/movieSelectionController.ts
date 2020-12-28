4ef3b4d8c65e63ffa9d0eafcad08f20fcfefa18f
philkazan <philkazan@gmail.com>
2020-12-28 16:59:34 -0500

    remove getMoviePoster function

--- src/controller/movieSelectionController.ts
+++ src/controller/movieSelectionController.ts
@@ -1,10 +1,6 @@
 import { MovieSelectionService } from '../service/movieSelectionService';
 
 export class MovieSelectionController { 
-    getMoviePoster(movieTitle: string) {
-        const service = new MovieSelectionService();
-        return service.getMoviePoster(movieTitle);
-    }
     getAvailableMovies() {
         const service = new MovieSelectionService();
         return service.getAvailableMovies();
