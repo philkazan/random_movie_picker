0ba045e5fe4555b428d57234b78607153d032eb9
philkazan <philkazan@gmail.com>
2020-12-28 16:59:01 -0500

    relocate movie class

---
+++ src/resource/movie.ts
@@ -0,0 +1,20 @@
+export class Movie {
+    private _title: string;
+    private _year: string;
+    private _poster: string;
+
+    constructor(title: string, year: string) {
+        this._title = title;
+        this._year = year;
+    }
+
+    get title() {
+        return this._title;
+    }
+    get year() {
+        return this._year;
+    }
+    get poster() {
+        return this._poster;
+    }
+}
