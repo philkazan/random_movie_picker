0b06f55faeffefc0c4f7b6e231e3b84a75f5d146
philkazan <philkazan@gmail.com>
2020-12-28 16:59:49 -0500

    rework getRandomMovie function

--- src/service/movieSelectionService.ts
+++ src/service/movieSelectionService.ts
@@ -1,40 +1,22 @@
-import { Movie } from '../abstraction/movie';
-import { Round } from '../abstraction/round';
-import { MovieSelectionResult } from '../abstraction/movieSelectionResult';
-import * as fs from 'fs';
+import { Movie } from '../resource/movie';
 
 export class MovieSelectionService { 
-    getMoviePoster(movieTitle: string) {
-            // return fs.readFileSync(`${__dirname}/movie_posters/${movieTitle}.jpg`);
-    }
-
     getAvailableMovies() {
         const inputFile: Movie[] = require('../input/movies.json');
         return inputFile;
     }
 
     getRandomMovie() {
-        const inputFile: Movie[] = require('../input/movies.json');
-        const availableMovies: Movie[] = [...inputFile]
-        const result = new MovieSelectionResult();
-        let roundNumber: number = 1;
+        const availableMovies: Movie[] = require('../input/movies.json');
+        const randomIndex = Math.floor(Math.random() * availableMovies.length)
+        return availableMovies[randomIndex];
+    }
 
-        while(availableMovies.length > 0) {
-            const round = new Round(roundNumber);
-            const randomIndex = Math.floor(Math.random() * availableMovies.length)
-            const randomMovie = availableMovies[randomIndex];
-            availableMovies.splice(randomIndex, 1);
+    addMovie() {
+        // not implemented
+    }
 
-            round.remainingMovies = [...availableMovies];
-            round.eliminatedMovie = randomMovie;
-            result.addToRounds(round);
-
-            if (availableMovies.length === 0) {
-                result.selectedMovie = randomMovie;
-            } else {
-                roundNumber++;
-            }
-        }
-        return result;
+    editMovie() {
+        // not implemented
     }
 } 
